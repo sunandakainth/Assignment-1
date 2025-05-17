4c554c911fcf18c228900e411f68470e5b25bcfb
sunandakainth <sunanda.aheer@gmail.com>
2025-05-17 18:46:25 +0530

    Add files via upload

---
+++ Assignment_1.py
@@ -0,0 +1,14 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Kaint
+#
+# Created:     17-05-2025
+# Copyright:   (c) Kaint 2025
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+first_name=input("Enter first name")
+second_name=input("Enter second name")
+print("Hello", first_name + " " + second_name, "! Welcome to Python Program.")
