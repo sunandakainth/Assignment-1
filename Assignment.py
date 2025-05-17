bdc84d8d5af482c8a905dba4c7891c3fc1785a7b
sunandakainth <sunanda.aheer@gmail.com>
2025-05-17 18:38:37 +0530

    Add files via upload

---
+++ Assignment.py
@@ -0,0 +1,22 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Kaint
+#
+# Created:     17-05-2025
+# Copyright:   (c) Kaint 2025
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+first_num= int(input("enter the first number"))
+second_num= int(input("enter the second number"))
+
+add=first_num + second_num
+print("the addition of two numbers are :",add)
+sub=first_num - second_num
+print("the subtraction of two numbers are :",sub)
+mul=first_num * second_num
+print("the multiplication of two numbers are :",mul)
+div=first_num / second_num
+print("the division of two numbers are :", div)
