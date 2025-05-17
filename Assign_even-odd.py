86dae70291ce1140be8b860fc4e71af8d11811dd
sunandakainth <sunanda.aheer@gmail.com>
2025-05-17 20:59:21 +0530

    Add files via upload

---
+++ Assign_even-odd.py
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Kaint
+#
+# Created:     17-05-2025
+# Copyright:   (c) Kaint 2025
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+Num= int(input("Enter the number :"))
+if (Num%2==0):
+    print("Number is Even:", Num)
+else:
+    print("Number is odd:", Num)
